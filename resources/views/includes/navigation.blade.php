59b18fb59fe5a6cb6c7028c4458d8b2f15ab397c
veegish <veegish240@gmail.com>
2017-01-09 23:12:44 +0400

    Fixed HTTPS URL errors

--- resources/views/includes/navigation.blade.php
+++ resources/views/includes/navigation.blade.php
@@ -71,8 +71,8 @@
               </ul>
               <ul class="navbar-right">
                 @if (Auth::guest())
-                    <li><a href="{{ url('/login') }}">Login</a></li>
-                    <li><a href="{{ url('/register') }}">Register</a></li>
+                    <li><a href="{{ URL::secure('/login') }}">Login</a></li>
+                    <li><a href="{{ URL::secure('/register') }}">Register</a></li>
                   @else
                       <li class="dropdown">
                           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
